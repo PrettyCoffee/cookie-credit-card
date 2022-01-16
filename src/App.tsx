545f25186cfa93930ad750d0a4d96b2abdd22b01
PrettyCoffee <jonathan-holz@outlook.com>
2022-01-16 23:22:34 +0100

    feat: Add App structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Card from "./Card"
+import Layout from "./Layout"
+import Theme from "./Theme"
+
+export const App = () => (
+  <Theme>
+    <Layout>
+      <Card />
+    </Layout>
+  </Theme>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
 import { render } from "preact"
 
-import styled from "styled-components"
-
-const H1 = styled.h1`
-  font-family: sans-serif;
-`
-
-const App = () => <H1>Hello from Preact and Typescript!</H1>
+import { App } from "./App"
 
 render(<App />, document.getElementById("root") as Element)
