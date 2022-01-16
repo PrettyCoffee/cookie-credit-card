d18e5ac901608f5c4b67a4146d679c99bebf223d
PrettyCoffee <jonathan-holz@outlook.com>
2022-01-16 20:41:40 +0100

    chore: Setup eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import {h, render} from 'preact';
+import { h, render } from "preact"
+
 import styled from "styled-components"
 
 const H1 = styled.h1`
   font-family: sans-serif;
 `
 
-const App = () => <H1>Hello from Preact and Typescript!</H1>;
+const App = () => <H1>Hello from Preact and Typescript!</H1>
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"))
