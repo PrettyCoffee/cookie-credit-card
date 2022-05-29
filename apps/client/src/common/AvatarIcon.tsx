2611b572a5702fb2a42ec412f854a09728b93b08
PrettyCoffee <jonathan-holz@outlook.com>
2022-05-29 14:59:48 +0200

    feat(client): Add avatar icon

---
+++ apps/client/src/common/AvatarIcon.tsx
@@ -0,0 +1,21 @@
+import { Icon, IconProps } from "@ccc/components"
+import styled, { css } from "styled-components"
+
+const Avatar = styled(Icon)`
+  ${({ theme: { color, shadow } }) => css`
+    height: 100px;
+    width: 100px;
+    display: flex;
+    border-radius: 50%;
+    border: 2px solid ${color.fg.base};
+
+    color: ${color.fg.alt};
+    filter: ${shadow.low(color.bg.alt, { usage: "drop-shadow" })};
+  `}
+`
+
+type AvatarIconProps = Omit<IconProps, "size">
+
+export const AvatarIcon = (props: AvatarIconProps) => (
+  <Avatar size="largest" {...props} />
+)
