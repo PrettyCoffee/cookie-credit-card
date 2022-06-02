dbdfa1cf5f23243f66148fc9ab797ceba5405fe7
PrettyCoffee <jonathan-holz@outlook.com>
2022-06-03 01:14:00 +0200

    feat(ThemeToggle): Add ThemeToggle component

--- apps/client/src/common/ThemeToggle.tsx
+++ apps/client/src/common/ThemeToggle.tsx
@@ -1,38 +1,19 @@
-import { Icon, VisuallyHidden } from "@ccc/components"
-import { Moon, Sun } from "react-feather"
-import styled, { css } from "styled-components"
+import { ThemeToggle as Toggle } from "@ccc/components"
+import styled from "styled-components"
 
 import { useDarkMode } from "../providers/DarkMode"
 
-const Button = styled.button`
-  ${({ theme: { color } }) => css`
-    position: fixed;
-    left: 0.5rem;
-    top: 0.5rem;
-    height: 2rem;
-    width: 2rem;
-
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-
-    color: ${color.fg.base};
-    background-color: transparent;
-    border: none;
-    outline: none;
-    cursor: pointer;
-  `}
+const Placement = styled.div`
+  position: fixed;
+  left: 0.5rem;
+  top: 0.5rem;
 `
 
 export const ThemeToggle = () => {
   const [darkMode, toggleDarkMode] = useDarkMode()
-
   return (
-    <Button role="switch" aria-checked={darkMode} onClick={toggleDarkMode}>
-      <Icon icon={darkMode ? Moon : Sun} />
-      <VisuallyHidden>
-        Toggle to {darkMode ? "light" : "dark"} colors
-      </VisuallyHidden>
-    </Button>
+    <Placement>
+      <Toggle inverted={darkMode} onInvert={toggleDarkMode} />
+    </Placement>
   )
 }
---
+++ packages/components/src/inputs/ThemeToggle.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useRef } from "preact/hooks"
+import { Moon, Sun } from "react-feather"
+import styled, { css, keyframes } from "styled-components"
+
+import { InvertProp, VisuallyHidden } from "../base"
+import { Icon } from "../primitives"
+
+const animateIn = keyframes`
+  from {
+    transform: rotate(-90deg);
+  } to {
+    transform: rotate(90deg);
+  }
+`
+
+const animateOut = keyframes`
+  from {
+    transform: rotate(90deg);
+  } to {
+    transform: rotate(270deg);
+  }
+`
+
+const Container = styled.button`
+  position: relative;
+  width: 2rem;
+  height: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  overflow: hidden;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 0.5rem;
+`
+
+const getAnimation = (active: boolean) => {
+  const outer = active ? animateIn : animateOut
+  const inner = active ? animateOut : animateIn
+  return css`
+    animation: 1s ${outer} forwards;
+    svg {
+      animation: 1s ${inner} forwards;
+    }
+  `
+}
+
+const Axis = styled.span<{ active: boolean; didMount: boolean }>`
+  ${({ active, didMount }) => css`
+    position: absolute;
+    bottom: -10px;
+    display: flex;
+    justify-content: space-between;
+    width: 50px;
+    ${!didMount &&
+    css`
+      transform: rotate(${active ? "90deg" : "-90deg"});
+    `}
+    ${didMount && getAnimation(active)}
+  `}
+`
+
+const useDidMountRef = () => {
+  const ref = useRef(false)
+  useEffect(() => {
+    ref.current = true
+  }, [])
+  return ref.current
+}
+
+interface ThemeToggleProps extends Required<InvertProp> {
+  onInvert: () => void
+}
+
+export const ThemeToggle = ({ inverted, onInvert }: ThemeToggleProps) => {
+  const didMount = useDidMountRef()
+  return (
+    <Container
+      role="switch"
+      aria-checked={inverted}
+      onClick={onInvert}
+      title="Toggle theme"
+    >
+      <Axis active={inverted} didMount={didMount}>
+        <Icon icon={Moon} />
+      </Axis>
+      <Axis active={!inverted} didMount={didMount}>
+        <Icon icon={Sun} />
+      </Axis>
+      <VisuallyHidden>
+        Toggle to {inverted ? "light" : "dark"} colors
+      </VisuallyHidden>
+    </Container>
+  )
+}
