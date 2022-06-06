9d95f02739d1ba9a6a44a815788741426d24166d
PrettyCoffee <jonathan-holz@outlook.com>
2022-06-06 23:32:40 +0200

    feat(IconButton): Add IconButton component

---
+++ packages/components/src/inputs/IconButton.tsx
@@ -0,0 +1,39 @@
+import styled, { css } from "styled-components"
+
+import { IconDefinition, VisuallyHidden } from "../base"
+import { Icon } from "../primitives"
+
+const Button = styled.button`
+  ${({ theme: { color } }) => css`
+    height: 2rem;
+    width: 2rem;
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 50%;
+    color: ${color.fg.button};
+
+    :hover {
+      background-color: ${color.bg.button};
+    }
+    :focus-visible {
+      outline: 1px solid ${color.fg.button};
+      background-color: ${color.bg.button};
+    }
+  `}
+`
+
+interface IconButtonProps {
+  icon: IconDefinition
+  label: string
+  onClick: () => void
+}
+
+export const IconButton = ({ icon, label, onClick }: IconButtonProps) => (
+  <Button title={label} onClick={onClick}>
+    <Icon icon={icon} />
+    <VisuallyHidden>{label}</VisuallyHidden>
+  </Button>
+)
